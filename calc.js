d451f5f0d832d262d1e22b24af75f23fb9642e19
Ajit1-mrk <108860453+Ajit1-mrk@users.noreply.github.com>
2022-07-07 13:23:41 +0530

    Calculator project

---
+++ calc.js
@@ -0,0 +1,47 @@
+function sum(){
+	
+	var fNumber = parseInt(document.getElementById('first').value);    
+	var sNumber = parseInt(document.getElementById('second').value); 
+	
+	var result= fNumber + sNumber;
+	
+	console.log("Addition is: "+result); 
+	
+}
+
+function sub(){
+	
+	var fNumber = parseInt(document.getElementById('first').value);    
+	var sNumber = parseInt(document.getElementById('second').value); 
+	
+	var result= fNumber - sNumber;
+	
+	console.log("Substraction  is: "+result); 
+	
+}
+
+function mul(){
+	
+	var fNumber = parseInt(document.getElementById('first').value);    
+	var sNumber = parseInt(document.getElementById('second').value); 
+	
+	var result= fNumber * sNumber;
+	
+	console.log("Multiplication  is: "+result); 
+	
+}
+
+function div(){
+	
+	var fNumber = parseInt(document.getElementById('first').value);    
+	var sNumber = parseInt(document.getElementById('second').value); 
+	
+	var result= fNumber / sNumber;
+	
+	console.log("Division  is: "+result); 
+	
+}
+
+
+
+
